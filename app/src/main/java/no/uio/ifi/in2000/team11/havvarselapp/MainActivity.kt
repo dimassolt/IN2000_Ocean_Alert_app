70695304ddb42ad3eeeddf02daa02827a9da512b
Sophie <sophirol@ulrik.uio.no>
2024-03-13 15:35:54 +0100

    Location forecast API, forklarer hvordan man henter data som kommentar øverst i LocationForecast data-klassen

--- app/src/main/java/no/uio/ifi/in2000/team11/havvarselapp/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team11/havvarselapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.team11.havvarselapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -10,13 +11,18 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.team11.havvarselapp.data.locationForecast.LocatinForecastRepositoryImpl
+import no.uio.ifi.in2000.team11.havvarselapp.data.locationForecast.LocationForecastDataSource
+import no.uio.ifi.in2000.team11.havvarselapp.data.locationForecast.LocationForecastRepository
 import no.uio.ifi.in2000.team11.havvarselapp.ui.theme.HavvarselAppTheme
 
 import no.uio.ifi.in2000.team11.havvarselapp.data.weather_current_waves.GripfilesDataSource
+import no.uio.ifi.in2000.team11.havvarselapp.ui.LocationForecast.LocationForecastViewModel
 
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +38,10 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
-            GribfilesDataSource()
+           // TestGribfilesDataSource()
+           // TestLocationForecastDataSource()
+           // TestLocationRepository()
+           // TestLocationForecastViewModel()
         }
     }
 }
@@ -40,18 +49,18 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GribfilesDataSource() {
+fun TestGribfilesDataSource() {
     val dataSource = GripfilesDataSource()
     val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(key1 = Unit) {
         coroutineScope.launch {
             try {
-                val weatherList = dataSource.fetchWeatherCurrentWaves2()
+                val weatherList = dataSource.fetchWeatherCurrentWaves()
 
                 weatherList.groupedWeatherCurrentWaves.forEach { (area, dataList) ->
                     dataList.forEach { data ->
-                        Log.e("WEATHER_DATA: ", "\n\nArea: $area, \nContent: ${data.params.content}, \nUpdated: ${data.updated}, \nURI: ${data.uri}")
+                        Log.e("WEATHER_DATA: ", " \nArea: $area, \nContent: ${data.params.content}, \nUpdated: ${data.updated}, \nURI: ${data.uri}\n ")
                     }
                 }
 
@@ -65,6 +74,98 @@ fun GribfilesDataSource() {
 }
 
 
+
+
+
+
+
+
+
+
+@Composable
+fun TestLocationForecastDataSource() {
+    val dataSource = LocationForecastDataSource()
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = Unit) {
+        coroutineScope.launch {
+            try {
+                val locationForecast = dataSource.fetchLocationForecast_LatAndLon( "59.9", "10.7")
+                Log.e("2-LOCATIONFORECAST_DATA: ", " \n\nDATA SOURCE: CURRENT WHEATER DATA\n " +
+                        "\nCordinates: ${locationForecast.geometry.coordinates} " +
+                        "\nTemperature: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.air_temperature} ${locationForecast.properties.meta.units.air_temperature}" +
+                        "\nUV-Index: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.ultraviolet_index_clear_sky} ${locationForecast.properties.meta.units.ultraviolet_index_clear_sky}" +
+                        "\nWind speed: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.wind_speed} ${locationForecast.properties.meta.units.wind_speed}" +
+                        "\nWind direction: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.wind_from_direction} ${locationForecast.properties.meta.units.wind_from_direction}\n\n")
+
+
+
+            } catch (e: Exception) {
+                // Logg eventuelle feil
+                Log.e("2-ERROR LOCATIONFORECAST_DATA", "feilmelding: ${e.message}")
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+@Composable
+fun TestLocationRepository() {
+    val repository = LocatinForecastRepositoryImpl()
+
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = Unit) {
+        coroutineScope.launch {
+            try {
+                val locationForecast = repository.getLocationForecast("59.9", "10.7")
+                if (locationForecast != null) {
+                    Log.e("REPOSITORY-LOCATIONFORECAST: ", " \n\nREPOSOTORY: CURRENT WHEATER DATA\n " +
+                            "\nCordinates: ${locationForecast.geometry.coordinates} " +
+                            "\nTemperature: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.air_temperature} ${locationForecast.properties.meta.units.air_temperature}" +
+                            "\nUV-Index: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.ultraviolet_index_clear_sky} ${locationForecast.properties.meta.units.ultraviolet_index_clear_sky}" +
+                            "\nWind speed: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.wind_speed} ${locationForecast.properties.meta.units.wind_speed}" +
+                            "\nWind direction: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.wind_from_direction} ${locationForecast.properties.meta.units.wind_from_direction}\n\n")
+                }
+
+
+            } catch (e: Exception) {
+                Log.e("ERROR forecast REPOSITORY", "Feil ved LocatinForecastRepositoryImpl()...", e)
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun TestLocationForecastViewModel() {
+    val forecastViewModel = LocationForecastViewModel()
+    val state = forecastViewModel.forecastInfo_UiState.collectAsState()
+
+    val coordinater = Pair("59.9", "10.7")
+    LaunchedEffect(coordinater) {
+        forecastViewModel.loadForecast(coordinater.first, coordinater.second)
+    }
+            try {
+                state.value?.let { locationForecast ->
+                    Log.e("ViewModel-LOCATIONFORECAST: ", " \n\nVIEW-MODEL: CURRENT WHEATER DATA\n " +
+                            "\nCordinates: ${locationForecast.geometry.coordinates} " +
+                            "\nTemperature: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.air_temperature} ${locationForecast.properties.meta.units.air_temperature}" +
+                            "\nUV-Index: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.ultraviolet_index_clear_sky} ${locationForecast.properties.meta.units.ultraviolet_index_clear_sky}" +
+                            "\nWind speed: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.wind_speed} ${locationForecast.properties.meta.units.wind_speed}" +
+                            "\nWind direction: ${locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.wind_from_direction} ${locationForecast.properties.meta.units.wind_from_direction}\n\n")
+
+                }
+
+            } catch (e: Exception) {
+                // Logg eventuelle feil
+                Log.e("ERROR VIEWMODEL FORECAST", "feilmelding: ${e.message}")
+                e.printStackTrace()
+            }
+
+
+}
 
 
 
--- app/src/main/java/no/uio/ifi/in2000/team11/havvarselapp/data/weather_current_waves/GripfilesDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team11/havvarselapp/data/weather_current_waves/GripfilesDataSource.kt
@@ -14,34 +14,39 @@ import io.ktor.client.*
 import com.google.gson.Gson
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.util.appendIfNameAbsent
+import no.uio.ifi.in2000.team11.havvarselapp.model.weather_current_waves.WeatherCurrentWaves_Objekt_liste
+import org.json.JSONObject
 
 
 class GripfilesDataSource {
 
     // API endepunkt fra MET for weather, currents og waves for 'ish' hele Norge
     private val url: String =
-        "https://gw-uio.intark.uh-it.no/in2000/weatherapi/gribfiles/1.1/available.json?content=waves"
+        "https://gw-uio.intark.uh-it.no/in2000/weatherapi/gribfiles/1.1/available.json"
 
     // HTTP client
     private val client = HttpClient {
         defaultRequest {
             url("https://gw-uio.intark.uh-it.no/in2000/")
             headers.appendIfNameAbsent("X-Gravitee-API-Key", "7c5b6de3-2539-4c5e-bfb3-ec6377399ece")
-
         }
-
 
         install(ContentNegotiation) {
             gson()
         }
     }
-    suspend fun fetchWeatherCurrentWaves2(): WeatherCurrentWaves_Objekt {
+
+    suspend fun fetchWeatherCurrentWaves(): WeatherCurrentWaves_Objekt {
 
         val liste_weatherCurrentWaves: List<WeatherCurrentWaves> = client.get(url).body()
         val groupedWeatherCurrentWaves = liste_weatherCurrentWaves.groupBy { it.labels.area }
         return WeatherCurrentWaves_Objekt(groupedWeatherCurrentWaves)
     }
+
+
+
 
 
 }

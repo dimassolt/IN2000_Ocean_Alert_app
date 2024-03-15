9d9040abaf6a767bec79c101bc9275c3e517c062
Max1kwow <maksymmo@ulrik.uio.no>
2024-03-15 13:53:54 +0100

    Working search bar

--- app/src/main/kotlin/no/uio/ifi/in2000/team11/havvarselapp/ui/map/SeaMap.kt
+++ app/src/main/kotlin/no/uio/ifi/in2000/team11/havvarselapp/ui/map/SeaMap.kt
@@ -1,5 +1,8 @@
 package no.uio.ifi.in2000.team11.havvarselapp.ui.map
 
+import android.content.Context
+import android.location.Address
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,8 +18,30 @@ import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.rememberCameraPositionState
 import no.uio.ifi.in2000.team11.havvarselapp.R
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import io.ktor.websocket.Frame
+
+import android.location.Geocoder
+import android.widget.Toast
+import com.google.maps.android.compose.CameraPositionState
+import java.io.IOException
+
+
 @Composable
 fun SeaMap() {
+    val textState = remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     // her trengs 'context' for å kunne hente utseende av kartet
     val context = LocalContext.current
@@ -30,23 +55,83 @@ fun SeaMap() {
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(oslo, 12f)
     }
-    // kart
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        properties = MapProperties(
-            // dette er utseende av kartet, som man finner i filen "mapstyle" i raw-mappen
-            mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context, R.raw.mapstyle),
 
-        )
-    ) {
-        // tegner opp et område i kartet //TODO slett dette
-        Polygon(
-            points = polygonCoordinates,
-            visible = true,
-            strokeColor = Color.Green,
-            fillColor = Color.Transparent,
-            strokeJointType = JointType.ROUND
-        )
+    Box(modifier = Modifier.fillMaxSize()) {
+        // kart
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            properties = MapProperties(
+                // dette er utseende av kartet, som man finner i filen "mapstyle" i raw-mappen
+                mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context, R.raw.mapstyle),
+
+                )
+        ) {
+            // tegner opp et område i kartet //TODO slett dette
+            Polygon(
+                points = polygonCoordinates,
+                visible = true,
+                strokeColor = Color.Green,
+                fillColor = Color.Transparent,
+                strokeJointType = JointType.ROUND
+            )
+        }
+
+        // TextField with the value from textState and an event handler to update textState
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = textState.value,
+                onValueChange = { newText ->
+                    textState.value = newText
+                },
+                // Optional parameters to customize the TextField
+                label = { Frame.Text("Enter text here") },
+                modifier = Modifier.padding(16.dp),
+
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        // Hide the keyboard when user presses enter
+                        getPosition(textState, context, cameraPositionState)
+                        keyboardController?.hide()
+                    }
+                )
+            )
+        }
     }
 }
+
+fun getPosition(placeName: MutableState<String>, context: Context, cameraPositionState: CameraPositionState){
+    val locationName = placeName.value
+    val geocoder = Geocoder(context)
+
+    try {
+        val addresses: List<Address>? = geocoder.getFromLocationName(locationName, 1)
+        if (addresses != null && addresses.isNotEmpty()) {
+            val address = addresses[0]
+            val latitude = address.latitude
+            val longitude = address.longitude
+
+            // TODO: remove or change text later
+            Toast.makeText(
+                context,
+                "Latitude: $latitude, Longitude: $longitude",
+                Toast.LENGTH_SHORT
+            ).show()
+            val searchLocation = LatLng(latitude, longitude)
+            val newPosition = CameraPosition.fromLatLngZoom(searchLocation, 12f)
+            cameraPositionState.position = newPosition
+        } else {
+            Toast.makeText(context, "Location not found", Toast.LENGTH_SHORT).show()
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+        Toast.makeText(context, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+    }
+}
+
